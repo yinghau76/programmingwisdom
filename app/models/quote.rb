99e695f54b5988f0e96e2bdb1f2c79645d441044
Patrick Tsai <yinghau76@gmail.com>
2013-12-21 00:55:24 +0800

    Reject possibly duplicated quote.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,21 @@
 class Quote < ActiveRecord::Base
   belongs_to :author
+  validate :possible_duplicated_quote
 
   def author_name
     author ? author.name : ''
+  end
+
+  def possible_duplicated_quote
+    longest_word = self.text.gsub(/[!?;,.]/, '').split.group_by(&:size).max.last
+    @possible_quotes = Quote.basic_search(longest_word)
+
+    @possible_quotes.each do |quote|
+      distance = Text::Levenshtein.distance(text, quote.text)
+      if distance < 16
+        errors.add(:text, "possibly duplicated to another quote (distance = #{distance}): #{quote.text}")
+        break
+      end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,55 +9,59 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713172043) do
+ActiveRecord::Schema.define(version: 20130713172043) do
 
-  create_table "authors", :force => true do |t|
-    t.string   "name",       :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+
+  create_table "authors", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "quotes", :force => true do |t|
+  create_table "quotes", force: true do |t|
     t.text     "text"
     t.string   "source"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "author_id"
   end
 
-  add_index "quotes", ["author_id"], :name => "index_quotes_on_author_id"
+  add_index "quotes", ["author_id"], name: "index_quotes_on_author_id", using: :btree
 
-  create_table "rails_admin_histories", :force => true do |t|
+  create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
